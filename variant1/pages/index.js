8b434dc94c21dadcd274ab923f38f2afdaf42b12
radekpadrta <75013107+radekpadrta@users.noreply.github.com>
2024-08-22 09:06:55 +0200

    Update index.js

--- variant1/pages/index.js
+++ variant1/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Dummy NextJS App A</h1>
+      <h1>Dummy NextJS App A_PR1</h1>
       <p>{message}</p>
       <p>Environment variable: {process.env.NEXT_PUBLIC_MESSAGE}</p>
     </div>
